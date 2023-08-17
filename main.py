51ba9fad03105b64066fd788b11c23d697dbba3a
castellino-u <castellino.u@gmail.com>
2023-08-17 10:17:37 -0300

    Realizado

---
+++ main.py
@@ -0,0 +1,43 @@
+#ejercicio 1- 
+numero1 = 23
+#ejercicio 2
+numero2 = 5.5
+#ejercicio 3
+suma = numero1 + numero2
+#ejercicio 4
+rest = numero1 - numero2
+div = numero1 / numero2
+mult = numero1 * numero2
+print(rest)
+print(div)
+print(mult)
+#ejercicio 5
+nombre = "Uriel Castellino"
+#ejercicio 6 
+precio = 29.99
+#Ejercicio 7
+desc =0.90
+#Ejercicio 8 
+precio_final = precio * desc
+#Ejercicio 9 
+cadena = "hola, buen dia.Hoy vamos a cocinar repollo, es como el pollo pero con un re"
+#Ejercicio 10 
+longitud = len(cadena)
+#Ejercicio 11
+precio= int(29.99)
+#Ejercicio 12
+nombre = "Uriel"
+apellido = "Castellino"
+nombre_completo = nombre + " " + apellido
+#Ejercicio 13 
+edad = 25
+edad_incre = edad + 5
+edad_dismi= edad - 10
+#Ejercicio 14 
+altura = 1.67
+altura = (altura * 4) / 3
+#Ejercicio 15
+nombre = "URIEL CASTELLINO" 
+nombre.lower()
+#Ejercicio 16
+nombre.capitalize()
