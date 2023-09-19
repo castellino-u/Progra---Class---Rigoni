db1dbc8b2273ddd34ed3fd022c1c4588e9cae94d
castellino-u <castellino.u@gmail.com>
2023-09-19 11:38:04 -0300

    Ejercicio 1 del practico de funciones en clase

---
+++ function_in_class.py
@@ -0,0 +1,23 @@
+def most(x,y):
+    if (x>y):
+        return x
+    else:
+        return y 
+
+def least(a,b):
+    if (x < y):
+        return x
+    else:
+        return y 
+
+
+# Programa principal
+
+x = int(input("Un numero: "))
+y = int(input("Otro número: "))
+
+print(most(x-3, least(x+2, y-5)))
+
+
+
+
